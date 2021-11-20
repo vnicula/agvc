5b88b31fd16ddc1254c0040e666ebc55e734f509
Gil Nicula <vnicula@gmail.com>
2021-11-20 01:23:44 -0800

    add in process pyaudio stream

--- test_main.py
+++ test_main.py
@@ -13,11 +13,9 @@ import matplotlib.pyplot as plt
 # from numba import jit
 import numpy as np
 import pyaudio
+import soundfile as sf
 import time
-# from librosa.filters import mel as librosa_mel_fn
-# from librosa.util import normalize
 import torch
-# import torchaudio
 
 import logging
 import util.dsp as dsp
@@ -51,14 +49,14 @@ my_config = Config('train_again-c4s.yml')
 model_state, step_fn = BaseAgent.build_model(my_config.build, mode='inference', device=device)
 model_state = BaseAgent.load_model(model_state, model_path, device=device)
 
-tgt = my_dsp.load_wav('data/wav48/celebs/amitheo.wav')
+tgt = my_dsp.load_wav('data/wav48/celebs/jeff_bezos.wav')
 print('target shape: ', tgt.shape)
 tgt_mel = my_dsp.wav2mel(tgt)
 print('tgt_mel shape:', tgt_mel.shape)
 
 def callback(in_data, frame_count, time_info, status):
     data = np.frombuffer(in_data, dtype=np.float32)
-    data = np.clip(data, -1.0, 1.0)
+    np.clip(data, -1.0, 1.0)
     src_mel = my_dsp.wav2mel(data)
     print(src_mel.shape)
 
@@ -98,7 +96,49 @@ def main(args=None):
     p.terminate()
 
 
+def main_inp(args=None):
+    same_seeds(961998)
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=attr_d['channels'],
+                    rate=attr_d["sampling_rate"],
+                    input=True,
+                    # output=True,
+                    frames_per_buffer=attr_d["segment_size"])
+
+    print("Starting to listen.")
+
+    out = np.empty((0,), dtype=np.float32)
+    data = stream.read(attr_d["segment_size"])
+    for i in range(0, 10):
+        data = np.frombuffer(data, dtype=np.float32)
+        data, _ = librosa.effects.trim(data, top_db=my_dsp.config['trim'])
+        np.clip(data, -1.0, 1.0)
+        src_mel = my_dsp.wav2mel(data)
+        print(src_mel.shape)
+
+        with torch.no_grad():
+            data = {
+                'source': {'mel': src_mel},
+                'target': {'mel': tgt_mel},
+            }
+            meta = step_fn(model_state, data)
+            dec = meta['dec']
+            y_out = my_dsp.mel2wav(dec)
+        
+        out = np.append(out, y_out[:attr_d["segment_size"]], axis=0)
+        print(out.shape)
+        # stream.write(y_out.tobytes(), attr_d["segment_size"])
+        
+        data = stream.read(attr_d["segment_size"])
+    
+    sf.write(file='10secs.wav', data=out, samplerate=attr_d["sampling_rate"])
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # parser.add_argument("model_path", type=Path)
-    main(**vars(parser.parse_args()))
+    main_inp(**vars(parser.parse_args()))
